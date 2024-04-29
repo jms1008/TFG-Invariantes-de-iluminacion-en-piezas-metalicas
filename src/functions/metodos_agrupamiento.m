746ae3fd7ca98b068291288b8d714d05f686842d
Jonás Martínez <jms1008@alu.ubu.es>
2024-04-29 13:30:06 +0200

    Refactorización y adaptación de metodos_agrupamiento.m
    He refactorizado el código para de esta manera eliminar código repetido. Además ahora  se considera también el que la imagen invariante sea una cadena vacía lo cual es útil a la hora de llamar a esta función sin necesidad de tener una imagen ground truth de la misma.

--- src/functions/metodos_agrupamiento.m
+++ src/functions/metodos_agrupamiento.m
@@ -3,34 +3,32 @@ function [imagen_final, porcentaje] = metodos_agrupamiento(opcion, imagen, img_g
         case 1
             % Elijo aplicar el método de K-Means a la imagen.
             [imagen_final, ~] = segmentar_imagen_KMeans(imagen, numClusters);
-            % Calculo el porcentaje de coincidencia con la imagen de verdad terreno.
-            porcentaje = calcular_coincidencia(imagen_final, img_ground_truth);
         case 2
             % Decido usar Fuzzy C-Means para segmentar la imagen.
             [imagen_final, ~] = segmentar_imagen_fuzzy_CMeans(imagen, numClusters);
-            % Vuelvo a calcular el porcentaje de coincidencia con la imagen base.
-            porcentaje = calcular_coincidencia(imagen_final, img_ground_truth);
         case 3
             % Opto por aplicar el método GMM para la segmentación.
             [imagen_final, ~] = segmentar_imagen_GMM(imagen, numClusters);
-            % Determino cómo de acertada fue la segmentación comparando con el ground truth.
-            porcentaje = calcular_coincidencia(imagen_final, img_ground_truth);
         case 4
             % Uso HMRF-EM-image de Quan Wang
             Y = rgb2gray(imagen);
-            Z = edge(Y,'canny',0.75);
+            Z = edge(Y, 'canny', 0.75);
             Y = double(Y);
-            Y = gaussianBlur(Y,3);
-            EM_iter=10; % max num of iterations
-            MAP_iter=10; % max num of iterations
-            [X, mu, sigma] = image_kmeans(Y,numClusters);
-            [X, mu, sigma] = HMRF_EM(X,Y,Z,mu,sigma,numClusters,EM_iter,MAP_iter);
-            imagen_final=(uint8(X*120));
-            % Determino cómo de acertada fue la segmentación comparando con el ground truth.
-            porcentaje = calcular_coincidencia(imagen_final, img_ground_truth);
+            Y = gaussianBlur(Y, 3);
+            EM_iter = 10; % max num of iterations
+            MAP_iter = 10; % max num of iterations
+            [X, mu, sigma] = image_kmeans(Y, numClusters);
+            [X, mu, sigma] = HMRF_EM(X, Y, Z, mu, sigma, numClusters, EM_iter, MAP_iter);
+            imagen_final = (uint8(X*120));
         otherwise
             % Si la opción no es reconocida, aviso al usuario.
             disp('Opción no válida');
             imagen_final = imagen; % Devuelvo la imagen original.
-            porcentaje = 0; % Indico que no hay coincidencia al no aplicar ningún método.
     end
+    % Determino cómo de acertada fue la segmentación comparando con el ground truth.
+    if isempty(img_ground_truth)
+        porcentaje = NaN; % No hay ground truth, porcentaje no aplicable
+    else
+        porcentaje = calcular_coincidencia(imagen_final, img_ground_truth);
+    end
+end
