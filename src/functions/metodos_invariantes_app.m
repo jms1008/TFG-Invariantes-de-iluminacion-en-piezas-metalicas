8a9de7d336136e38f5c2a8f970aca5dfea5bc335
Jonás Martínez <jms1008@alu.ubu.es>
2024-06-03 18:58:35 +0200

    Corrección de errores y actualización de documentación dentro de la app
    He corregido pequeños errores para la correcta ejecución del código y he actualizado la documentación para que sea fiel al estado actual de la aplicación además de pequeños errores que he podido ir encontrando. #51

--- src/functions/metodos_invariantes_app.m
+++ src/functions/metodos_invariantes_app.m
@@ -5,7 +5,7 @@ function imagen_resultado = metodos_invariantes_app(opcion, imagen_tres_canales)
         case 2
             imagen_resultado = maddern_transform(imagen_tres_canales, calcular_alpha(620, 540, 470));
         case 3
-            imagen_resultado = krajnik_transform(imagen_tres_canales, calcular_theta_krajnik(app.imagen_tres_canales));
+            imagen_resultado = krajnik_transform(imagen_tres_canales, calcular_theta_krajnik(imagen_tres_canales));
         case 4
             imagen_resultado = upcroft_transform(imagen_tres_canales, calcular_alpha(620, 540, 470));
         case 5
